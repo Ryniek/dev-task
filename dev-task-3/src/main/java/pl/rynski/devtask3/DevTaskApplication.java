3e7cec80d689354a8c9b64a50f4b4930d1ef059c
Michał Ryński <michalrynski96@gmail.com>
2022-12-28 15:20:14 +0100

    Third task done

--- dev-task-3/src/main/java/pl/rynski/devtask3/DevTaskApplication.java
+++ dev-task-3/src/main/java/pl/rynski/devtask3/DevTaskApplication.java
@@ -1,8 +1,28 @@
 package pl.rynski.devtask3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class DevTaskApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int connectionsInGraph = Integer.valueOf(in.readLine());
 		
+		Map<Integer, Integer> graph = new TreeMap<Integer, Integer>();
+		for(int i = 0; i < connectionsInGraph; i++) {
+			String[] points = in.readLine().split(" ");
+			graph.put(Integer.valueOf(points[0]), Integer.valueOf(points[1]));
+		}
+		
+		int numberOfGraphs = 0;
+		for (Integer key : graph.keySet()) {
+	        if(!graph.containsValue(key) || !graph.containsKey(key)) numberOfGraphs++;
+	    }
+		
+		System.out.println(numberOfGraphs);
 	}
 }
