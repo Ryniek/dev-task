400d73198687318663f8152cf00c91acb9145f5d
Michał Ryński <michalrynski96@gmail.com>
2022-12-28 13:34:54 +0100

    Second task done

--- dev-task-2/src/main/java/pl/rynski/devtask2/DevTaskApplication.java
+++ dev-task-2/src/main/java/pl/rynski/devtask2/DevTaskApplication.java
@@ -1,9 +1,29 @@
 package pl.rynski.devtask2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class DevTaskApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		List<Integer> numbers = Arrays.stream(in.readLine().split(" ")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+		List<Integer> lowerNumbers = numbers.stream().filter((number) -> number <= 6).sorted().toList();
+		List<Integer> higherNumbers = numbers.stream().filter((number) -> number >= 7).sorted(Collections.reverseOrder()).toList();
+		
+		for(Integer lowerNumber: lowerNumbers) {
+			Optional<Integer> higherNumber = higherNumbers.stream().filter(num -> (13 - num == lowerNumber)).findFirst();
+			if(higherNumber.isPresent()) {
+				System.out.println(lowerNumber + " " + higherNumber.get());
+				continue;
+			}
+		}
 	}
 
 }
