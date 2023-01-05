1494653036eab0047d4792ae49cce4d0c31193fa
Michał Ryński <michalrynski96@gmail.com>
2023-01-05 18:34:04 +0100

    Task 3 - fix

--- dev-task-3/DevTaskApplication.java
+++ dev-task-3/DevTaskApplication.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -17,20 +18,27 @@ public class DevTaskApplication {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int connectionsInGraph = Integer.valueOf(in.readLine());
 		
-		Map<Integer, Set<Integer>> graphs = new HashMap<>(); 
+		List<Set<Integer>> graphs = new ArrayList<>();
 		
 		for(int i = 0; i < connectionsInGraph; i++) {
 			String[] input = in.readLine().split(" ");
-			boolean connected = false;
 			Set<Integer> points = new HashSet<>(Arrays.asList(Integer.valueOf(input[0]), Integer.valueOf(input[1])));
-			for(Map.Entry<Integer, Set<Integer>> entry : graphs.entrySet()) {
-				if(entry.getValue().stream().anyMatch(points::contains)) {
-					entry.getValue().addAll(points);
-					connected = true;
-					break;
+			List<Integer> indexesOfGraphsToConnect = new ArrayList<>();
+			for(int m = 0; m < graphs.size(); m++) {
+				if(graphs.get(m).stream().anyMatch(points::contains)) {
+					indexesOfGraphsToConnect.add(m);
 				}
 			}
-			if(connected == false) graphs.put(i, points);
+			if(indexesOfGraphsToConnect.size() == 0) {
+				graphs.add(points);
+			} 
+			else {
+				Set<Integer> newGraph = new HashSet<>(points);
+				for(int j = indexesOfGraphsToConnect.size() - 1; j >= 0; j--) {
+					newGraph.addAll(graphs.remove(j));
+				}
+				graphs.add(newGraph);
+			}
 		}
 		
 		System.out.println(graphs.size());
